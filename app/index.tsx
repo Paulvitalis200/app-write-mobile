2f91f29d9637a1ac0e83fbce8cacd18c24c57b1e
Paul Otieno <paul.otieno@gitlab.com>
2024-08-01 09:33:15 +0300

    Update font weight

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="flex-1 items-center justify-center bg-white font-pblack">
       <Text className="text-3xl">Finesse</Text>
     </View>
   );

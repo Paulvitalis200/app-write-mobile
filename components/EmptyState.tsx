c4b57c73cc79b65a06634d092bf6b759711ad093
Paul Otieno <paul.otieno@gitlab.com>
2024-09-03 01:53:22 +0300

    Fix router import

--- components/EmptyState.tsx
+++ components/EmptyState.tsx
@@ -2,6 +2,7 @@ import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { images } from '@/constants'
 import CustomButton from './CustomButton'
+import {  router } from 'expo-router'
 
 const EmptyState = ({title, subtitle}) => {
   return (
